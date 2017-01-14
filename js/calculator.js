8f12dca95760b2b13d68f01119774e622bcd0297
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-01-14 14:47:54 -0500

    Buttons append to input text box

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,12 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-}
+// }
 
 function calculate() {
+	alert('calculating');
+}
 
+function addValueToEquation(newValue) {
+	currentEquation = document.getElementById('results-box');
+	currentEquation.value = (currentEquation.value + newValue);
 }
